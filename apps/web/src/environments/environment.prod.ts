a72c3f5243270de4aa12f140b36245cd290ff076
jasonhulbert <hulbert.jason@gmail.com>
2026-01-29 13:50:32 -0500

    chore(coming-soon): Add maintenance mode configuration and environment variable setup script

--- apps/web/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  comingSoon: true,
-  supabaseUrl: '', // Will be set during deployment or via environment variables
-  supabaseAnonKey: '', // Will be set during deployment or via environment variables
-};
--- apps/web/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  comingSoon: false,
-  supabaseUrl: 'http://127.0.0.1:54321',
-  supabaseAnonKey: 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH',
-};
